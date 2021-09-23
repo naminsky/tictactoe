a76263311fb10458e38faec82cea21b4a4bb0521
Hiroaki Namikata <hiroaki733@outlook.jp>
2021-09-24 03:09:43 +0900

    wip

--- game.js
+++ game.js
@@ -4,47 +4,24 @@ export class TicTacToe {
     }
 
     reset() {
-        this.turn = "cross"
         this.states = [
             "", "", "",
             "", "", "",
             "", "", ""
         ]
+        this.over = false
+        this.winner = ""
     }
-
+    
     changeState(cell_id) {
-        this.states[cell_id] = this.turn
-        return
-    }
+        this.states[cell_id] = this.active_turn
 
-    changeTurn() {
-        if (this.turn === "cross") {
-            this.turn = "nought"
-        } else {
-            this.turn = "cross"
+        // do not judge on turn 1-4
+        if (this.available_moves.length > 4) {
+            return
         }
-        return
-    }
 
-    get isCrossTurn() {
-        if (this.turn === "cross") {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    get availableMoves() {
-        return this.states.reduce((results, state, index) => {
-            if (!state) {
-                // console.log(state)
-                results.push(index)
-            }
-            return results
-        }, [])
-    }
-
-    get winner() {
+        // judge
         const board_lines = [
             // horizontally
             [this.states[0], this.states[1], this.states[2]],
@@ -60,23 +37,52 @@ export class TicTacToe {
             [this.states[0], this.states[4], this.states[8]],
             [this.states[2], this.states[4], this.states[6]]
         ]
-
+    
         // console.log(board_lines)
-
+    
         for (let i = 0; i < board_lines.length; i++) {
             if (board_lines[i].every(state => state === "cross")) {
-                return "cross"
+                this.over = true
+                this.winner = "cross"
             } else if (board_lines[i].every(state => state === "nought")) {
-                return "nought"
+                this.over = true
+                this.winner = "nought"
             }
         }
-
+    
+        if (this.over === false && this.available_moves.toString() === [].toString()) {
+            console.log("draw")
+            this.over = true
+        }
         return
+    }
+
+    get active_turn() {
+        const crosses_count = this.states.filter(state => state === "cross").length
+        const noughts_count = this.states.filter(state => state === "nought").length
+        if (crosses_count === noughts_count) {
+            return "cross"
+        } else {
+            return "nought"
+        }
+    }
+
+    get available_moves() {
+        return this.states.reduce((results, state, index) => {
+            // console.log(state)
+            if (!state) {
+                results.push(index)
+            }
+            return results
+        }, [])
     }
 }
 
 export class CPU {
-    constructor() {}
+    constructor() {
+        let choice = 0
+    }
+
     score(game) {
         if (game.winner === "cross") {
             return 10
@@ -88,11 +94,32 @@ export class CPU {
     }
 
     minimax(game) {
-        if (game.winner === "cross" || game.winner === "nought") {
+        if (game.over) {
             return this.score(game)
         }
 
-        scores = [];
-        moves = [];
+        const scores = [];
+        const moves = [];
+
+        for (const move of game.available_moves) {
+            const possible_game = new TicTacToe
+            possible_game.changeState(move)
+            scores.push(this.minimax(possible_game))
+            moves.push(move)
+        }
+
+        if (game.active_turn === "cross") {
+            // max calculation
+            const max_score = Math.max(...scores)
+            const max_score_index = scores.findIndex(score => score === max_score)
+            choice = moves[max_score_index];
+            return max_score
+        } else {
+            // min calculation
+            const min_score = Math.min(...scores)
+            const min_score_index = scores.findIndex(score => score === min_score)
+            choice = moves[min_score_index];
+            return min_score
+        }
     }
 }
--- test-for-game.js
+++ test-for-game.js
@@ -16,42 +16,169 @@ const printTest = (output, expected) => {
 }
 
 const gameTest = (game,
-                  ex_turn,
+                  ex_over,
+                  ex_active_turn,
                   ex_states,
-                  ex_isCrossTurn,
-                  ex_availableMoves,
+                  ex_available_moves,
                   ex_winner
     ) => {
+        const results = [];
+
         console.log("--------------------")
 
-        console.log("[.turn]")
-        printTest(game.turn, ex_turn)
+        console.log("[.over]")
+        results.push(printTest(game.over, ex_over))
+        
+        console.log("[.active_turn()]")
+        results.push(printTest(game.active_turn, ex_active_turn))
 
         console.log("[.states]")
-        printTest(game.states, ex_states)
-        
-        console.log("[.isCrossTurn()]")
-        printTest(game.isCrossTurn, ex_isCrossTurn)
+        results.push(printTest(game.states, ex_states))
 
-        console.log("[.availableMoves()]")
-        printTest(game.availableMoves, ex_availableMoves)
+        console.log("[.available_moves()]")
+        results.push(printTest(game.available_moves, ex_available_moves))
 
-        console.log("[.winner()]")
-        printTest(game.winner, ex_winner)
+        console.log("[.winner]")
+        results.push(printTest(game.winner, ex_winner))
 
         console.log("--------------------")
         
-        return
+        return results
     }
 
-const game = new TicTacToe
+let results = [];
 
-console.log("###initial status###")
-gameTest(game,
-         "cross",
-         ["", "", "", "", "", "", "", "", ""],
-         true,
-         [0, 1, 2, 3, 4, 5, 6, 7, 8],
-         undefined
-        )
+console.log("###game_1: initial status###")
+const game_1 = new TicTacToe
+results.push(gameTest(game_1,
+                      false,
+                      "cross",
+                      ["", "", "", "", "", "", "", "", ""],
+                      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                      ""
+    ))
 console.log("")
+
+console.log("###game_1: turn 1###")
+game_1.changeState(0)
+results.push(gameTest(game_1,
+                      false,
+                      "nought",
+                      ["cross", "", "", "", "", "", "", "", ""],
+                      [1, 2, 3, 4, 5, 6, 7, 8],
+                      ""
+    ))
+console.log("")
+
+console.log("###game_1: turn 2###")
+game_1.changeState(4)
+results.push(gameTest(game_1,
+                      false,
+                      "cross",
+                      ["cross", "", "", "", "nought", "", "", "", ""],
+                      [1, 2, 3, 5, 6, 7, 8],
+                      ""
+    ))
+console.log("")
+
+console.log("###game_1: cross player win###")
+game_1.changeState(2)
+game_1.changeState(3)
+game_1.changeState(1)
+results.push(gameTest(game_1,
+                      true,
+                      "nought",
+                      ["cross", "cross", "cross", "nought", "nought", "", "", "", ""],
+                      [5, 6, 7, 8],
+                      "cross"
+    ))
+console.log("")
+
+console.log("###game_2: initial status###")
+const game_2 = new TicTacToe
+results.push(gameTest(game_2,
+                      false,
+                      "cross",
+                      ["", "", "", "", "", "", "", "", ""],
+                      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                      ""
+    ))
+console.log("")
+
+console.log("###game_2: nought player win###")
+game_2.changeState(0)
+game_2.changeState(1)
+game_2.changeState(2)
+game_2.changeState(4)
+game_2.changeState(6)
+game_2.changeState(7)
+results.push(gameTest(game_2,
+    true,
+    "cross",
+    ["cross", "nought", "cross", "", "nought", "", "cross", "nought", ""],
+    [3, 5, 8],
+    "nought"
+))
+console.log("")
+
+console.log("###game_3: initial status###")
+const game_3 = new TicTacToe
+results.push(gameTest(game_3,
+                      false,
+                      "cross",
+                      ["", "", "", "", "", "", "", "", ""],
+                      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                      ""
+    ))
+console.log("")
+
+console.log("###game_3: nought player win###")
+game_3.changeState(0)
+game_3.changeState(1)
+game_3.changeState(2)
+game_3.changeState(4)
+game_3.changeState(6)
+game_3.changeState(7)
+results.push(gameTest(game_3,
+    true,
+    "cross",
+    ["cross", "nought", "cross", "", "nought", "", "cross", "nought", ""],
+    [3, 5, 8],
+    "nought"
+))
+console.log("")
+
+console.log("###game_4: initial status###")
+const game_4 = new TicTacToe
+results.push(gameTest(game_4,
+                      false,
+                      "cross",
+                      ["", "", "", "", "", "", "", "", ""],
+                      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                      ""
+    ))
+console.log("")
+
+console.log("###game_4: nought player win###")
+game_4.changeState(0)
+game_4.changeState(1)
+game_4.changeState(2)
+game_4.changeState(4)
+game_4.changeState(3)
+game_4.changeState(5)
+game_4.changeState(7)
+game_4.changeState(6)
+game_4.changeState(8)
+results.push(gameTest(game_4,
+    true,
+    "nought",
+    ["cross", "nought", "cross", "cross", "nought", "nought", "nought", "cross", "cross"],
+    [],
+    ""
+))
+console.log("")
+
+const tmp = results.flat()
+const pass = tmp.filter(bool => bool === true)
+
+console.log(`results: ${pass.length} / ${tmp.length} (pass / total)`)
